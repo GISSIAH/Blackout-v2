10ade1d60986dae2600ec66c1c3e9315e928348c
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-05-10 13:33:09 +0200

    Added notification storage on device v2

--- app/src/main/java/com/example/bottomnavbardemo/screens/Notifications.kt
+++ app/src/main/java/com/example/bottomnavbardemo/screens/Notifications.kt
@@ -1,7 +1,6 @@
 package com.example.bottomnavbardemo.screens
 
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,7 +16,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
-import androidx.room.Room
 import com.example.bottomnavbardemo.database.AppDatabase
 import com.example.bottomnavbardemo.models.notification
 import com.example.bottomnavbardemo.ui.theme.Gray900
